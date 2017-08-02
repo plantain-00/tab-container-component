abfdca95f58c9b053140f5c04a09ffc95b7a912b
york yao <yaoao12306@outlook.com>
2017-08-02 18:56:28 +0800

    update webpack config

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -8,7 +8,7 @@ module.exports = {
     `lessc src/tab-container.less > dist/tab-container.css`,
     `cleancss -o dist/tab-container.min.css dist/tab-container.css`,
     `cleancss -o demo/index.bundle.css dist/tab-container.min.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css`,
-    `webpack --config demo/webpack.config.js`,
+    `webpack --display-modules --config demo/webpack.config.js`,
     `rev-static --config demo/rev-static.config.js`
   ],
   lint: [
