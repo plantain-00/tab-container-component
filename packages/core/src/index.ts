4f8480dbe2f79efe4623aec9eb435286cb778c0e
york yao <yaoao12306@outlook.com>
2018-01-21 10:34:08 +0800

    chore: update dependencies

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,10 +4,8 @@
 export type TabContainerData = {
   isActive: boolean;
   title?: string;
-    // tslint:disable-next-line:ban-types
   titleComponent?: string | Function;
   titleData?: any; // the data will be passed to the titleComponent as `data` props
-    // tslint:disable-next-line:ban-types
   component: string | Function;
   data: any; // the data will be passed to the component as `data` props
   canClose?: boolean;
