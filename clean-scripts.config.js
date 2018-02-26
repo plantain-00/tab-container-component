fc334dddac892787986717ae682d72581ba0ed61
york yao <yaoao12306@outlook.com>
2018-02-27 07:53:33 +0800

    build: improve scripts

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -6,16 +6,17 @@ const lessFiles = `"packages/core/src/**/*.less"`
 const jsFiles = `"*.config.js" "spec/**/*.config.js"`
 
 const vueTemplateCommand = `file2variable-cli --config packages/vue/src/file2variable.config.js`
-const tscSrcCommand = [
-  `tsc -p packages/core/src`,
-  `tsc -p packages/vue/src`,
-  `tsc -p packages/react/src`
-]
-const tscDemoCommand = [
-  `tsc -p packages/vue/demo`,
-  `tsc -p packages/react/demo`
-]
-const webpackCommand = `webpack`
+
+const tscCoreSrcCommand = `tsc -p packages/core/src`
+const tscVueSrcCommand = `tsc -p packages/vue/src`
+const tscReactSrcCommand = `tsc -p packages/react/src`
+
+const tscVueDemoCommand = `tsc -p packages/vue/demo`
+const tscReactDemoCommand = `tsc -p packages/react/demo`
+
+const webpackVueCommand = `webpack --config packages/vue/demo/webpack.config.js`
+const webpackReactCommand = `webpack --config packages/react/demo/webpack.config.js`
+
 const revStaticCommand = `rev-static`
 const cssCommand = [
   `lessc packages/core/src/tab-container.less -sm=on > packages/core/src/tab-container.css`,
@@ -28,10 +29,20 @@ module.exports = {
   build: [
     {
       js: [
-        vueTemplateCommand,
-        tscSrcCommand,
-        tscDemoCommand,
-        webpackCommand
+        tscCoreSrcCommand,
+        {
+          vue: [
+            vueTemplateCommand,
+            tscVueSrcCommand,
+            tscVueDemoCommand,
+            webpackVueCommand
+          ],
+          react: [
+            tscReactSrcCommand,
+            tscReactDemoCommand,
+            webpackReactCommand
+          ]
+        }
       ],
       css: cssCommand,
       clean: `rimraf "packages/@(core|vue|react|angular)/demo/**/@(*.bundle-*.js|*.bundle-*.css)"`
@@ -57,10 +68,14 @@ module.exports = {
     less: `stylelint --fix ${lessFiles}`
   },
   watch: {
-    vue: `${vueTemplateCommand} --watch`,
-    src: `${tscSrcCommand} --watch`,
-    demo: `${tscDemoCommand} --watch`,
-    webpack: `${webpackCommand} --watch`,
+    vueTemplateCommand: `${vueTemplateCommand} --watch`,
+    tscCoreSrcCommand: `${tscCoreSrcCommand} --watch`,
+    tscVueSrcCommand: `${tscVueSrcCommand} --watch`,
+    tscReactSrcCommand: `${tscReactSrcCommand} --watch`,
+    tscVueDemoCommand: `${tscVueDemoCommand} --watch`,
+    tscReactDemoCommand: `${tscReactDemoCommand} --watch`,
+    webpackVueCommand: `${webpackVueCommand} --watch`,
+    webpackReactCommand: `${webpackReactCommand} --watch`,
     less: () => watch(['src/**/*.less'], [], () => executeScriptAsync(cssCommand)),
     rev: `${revStaticCommand} --watch`
   },
