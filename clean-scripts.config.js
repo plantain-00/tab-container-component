3959a377cd438dbf9013f7737f2956831bb5d6f1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-26 00:11:07 +0000

    Deploy plantain-00/tab-container-component to github.com/plantain-00/tab-container-component.git:gh-pages

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -60,7 +60,7 @@ module.exports = {
     export: `no-unused-export ${tsFiles} ${lessFiles} --exclude "src/compiled/**/*"`,
     commit: `commitlint --from=HEAD~1`,
     markdown: `markdownlint README.md`,
-    typeCoverage: 'lerna exec -- type-coverage -p src'
+    typeCoverage: 'lerna exec -- type-coverage -p src --strict'
   },
   test: [
     'tsc -p spec',
